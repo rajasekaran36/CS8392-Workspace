d4d96873a8bec52dc5f461ae44ec86172cc00e80
Rajasekaran S <proffraja@gmail.com>
2018-09-27 13:21:16 +0530

    Last Update 27-09-2018 13:21:11.97

--- L6-PrintAreaofDiffShape/src/Main.java
+++ L6-PrintAreaofDiffShape/src/Main.java
@@ -24,13 +24,51 @@ class Rectangle extends Shape{
 	}
 	
 }
+
+class Triangle extends Shape{
+
+	public Triangle(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	void printArea() {
+		
+		double area;
+		area = (0.5)*x*y;
+		System.out.println("Area of Rectangle is "+area);
+	}
+	
+}
+
+class Circle extends Shape{
+
+	public Circle(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	void printArea() {
+		
+		double area;
+		area = (Math.PI)*x*x;
+		System.out.println("Area of Rectangle is "+area);
+	}
+	
+}
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		Rectangle r1 = new Rectangle(10,20);
-		Shape r2 = new Rectangle(50, 100);
+		Triangle t1 = new Triangle(10, 20);
+		Circle c1 = new Circle(10,10);
 		r1.printArea();
-		r2.printArea();
+		t1.printArea();
+		c1.printArea();
 	}
 
 }
