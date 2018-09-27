eac25c98ca6dd5cf1de6339c9526dafc71286e5a
Rajasekaran S <proffraja@gmail.com>
2018-09-27 10:16:20 +0530

    Last Update 27-09-2018 10:16:19.40

--- L4-StackADTImpl/src/Main.java
+++ L4-StackADTImpl/src/Main.java
@@ -12,14 +12,22 @@ class MyStack implements StackOps{
 	int top = -1;
 	@Override
 	public void push(int anElement) {
+		try {
 		top++;
 		data[top] = anElement;
+		System.out.println(anElement+" is pushed");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("You can't push try pop");
+			top--;
+		}
 	}
 
 	@Override
 	public int pop() {
-		
-		return 0;
+		int poped_element = data[top];
+		top--;
+		System.out.println("Current Top"+data[top]);
+		return poped_element;
 	}
 
 	@Override
@@ -35,5 +43,13 @@ class MyStack implements StackOps{
 	
 }
 public class Main {
-
+	public static void main(String[] args) {
+		MyStack aStack = new MyStack();
+		aStack.push(100);
+		aStack.push(200);
+		aStack.push(300);
+		aStack.push(400);
+		aStack.push(500);
+		aStack.push(600);
+	}
 }
